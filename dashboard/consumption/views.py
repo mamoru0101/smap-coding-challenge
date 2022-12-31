8f2c2b23ea79a01da0731c7e1dc684fcdf5d63db
kdm0101 <77522275+kdm0101@users.noreply.github.com>
2022-12-31 22:41:26 +0900

    ## Challenge 1 - fix

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -4,15 +4,31 @@ from __future__ import unicode_literals
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db import models
 from django.db.models.functions import TruncDate
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from consumption.models import ConsumptionData, UserData
 
 
 def summary(request):
+    users = all_users()
+    paginator = Paginator(users, 20)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        pages = paginator.page(page_number)
+    except PageNotAnInteger:
+        pages = paginator.page(1)
+    except EmptyPage:
+        pages = paginator.page(1)
+
     context = {
-        'users': all_users(),
-        'aggregate_consumption': aggregate_consumption()
+        'aggregate_consumption': aggregate_consumption(),
+        'users': pages.object_list,
+        "pages": pages,
+        'paginator': paginator,
+        'is_paginated': pages.has_other_pages(),
+        'object_list': pages.object_list,
     }
     return render(request, 'consumption/summary.html', context)
 
@@ -29,41 +45,55 @@ def aggregate_consumption():
 
 
 def all_users():
-    return UserData.objects.all().annotate(
-        total=models.Sum("consumptiondata__consumption"),
-        average=models.Avg("consumptiondata__consumption")
-    )
+    return UserData.objects.all().order_by("id")
 
 
 def detail(request, user_id):
     user = get_object_or_404(UserData, pk=user_id)
-    date_list = user.consumptiondata_set.all() \
-        .annotate(date=TruncDate("datetime")) \
-        .values_list("date", flat=True) \
-        .order_by("-date") \
-        .distinct()
-    date_list = [str(d) for d in date_list]
-    current_date = request.GET.get('current_date', date_list[0])
+    try:
+        date_list = user.consumptiondata_set.all() \
+            .annotate(date=TruncDate("datetime")) \
+            .values_list("date", flat=True) \
+            .order_by("-date") \
+            .distinct()
+        date_list = [str(d) for d in date_list]
 
-    date_nation(date_list, current_date)
+        current_date = request.GET.get('current_date', date_list[0])
 
-    user_consumption = user.consumptiondata_set.filter(datetime__date=current_date).order_by("datetime")
+        prev_date, next_date = get_prev_and_next_date(date_list, current_date)
+
+        user_consumption = user.consumptiondata_set.filter(datetime__date=current_date).order_by("datetime")
+    except:
+        raise Http404()
 
     context = {
         "user": user,
         "consumption_list": user_consumption,
         'date_list': date_list,
-        'current_date': current_date
+        'current_date': current_date,
+        'prev_date': prev_date,
+        'next_date': next_date
     }
     return render(request, 'consumption/detail.html', context)
 
 
-def date_nation(date_list, current_date, ):
+def get_prev_and_next_date(date_list, current_date):
+    """
+    :param date_list: list
+    :param current_date: str
+
+    :return:
+    prev_date: str|None
+    next_date: str|None
+    """
+
+    if len(date_list) < 1:
+        return None, None
+
+    date_list = [None] + date_list + [None]
     date_index = date_list.index(current_date)
-    if len(date_list) < date_index:
-        pass
-    print(date_list)
-    print(date_index)
+    next_date, c, prev_date = date_list[(date_index - 1):(date_index + 2)]
+    return prev_date, next_date
 
 
 def detail_(request, user_id):
